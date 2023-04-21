d21667adb725fb5dbb2d4f6812e2311f0a6f3279
git2kodama <ebook2kodama@gmail.com>
2023-04-22 05:35:41 +0900

    ファイルの保存機能のドラフト

--- components/ImportExportButtons.tsx
+++ components/ImportExportButtons.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useConfigurationsContext } from './ConfigurationsComponent';
+
+function saveJSONToFile(jsonString: string, filename: string): void {
+  const blob = new Blob([jsonString], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = filename;
+  link.style.display = "none";
+  document.body.appendChild(link);
+  link.click();
+  URL.revokeObjectURL(url);
+  document.body.removeChild(link);
+}
 
 const ImportExportButtons = () => {
+  const {bEditMode} = useConfigurationsContext(); 
+  const { configurations } = useConfigurationsContext();
+
+  const handleExportConfiguration = () => {
+    const jsonString = JSON.stringify(configurations,null,2);
+    saveJSONToFile(jsonString, "configurations.json");    
+  }
+
   return (
     <>
       <div className="mb-3">
         <label htmlFor="import-json" className="form-label">Import JSON:</label>
         <input className="form-control" type="file" id="import-json" accept=".json" />
       </div>
-      <button className="btn btn-success">Export JSON</button>
+      <button className="btn btn-success" onClick={handleExportConfiguration} disabled={bEditMode}>Export JSON</button>
     </>
   );
 };
